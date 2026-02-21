b65e4a23371e712b0f3058e8f501fa5ab815e317
Nayan Dey <ndeynayan123@gmail.com>
2026-02-22 01:11:18 +0530

    Implement button toggling functionality for job application filters

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,2 +1,17 @@
 console.log("toggle");
 
+const allFilterBtn = document.getElementById("all-filter-btn")
+const interviewFilterBtn = document.getElementById("interview-filter-btn")
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn")
+// console.log(rejectedFilterBtn)
+
+// button toggle
+function toggleStyle(id){
+    allFilterBtn.classList.remove("btn-primary")
+    interviewFilterBtn.classList.remove("btn-primary")
+    rejectedFilterBtn.classList.remove("btn-primary")
+
+    const selectedBtn = document.getElementById(id);
+    console.log(selectedBtn);
+    selectedBtn.classList.add("btn-primary")
+}
