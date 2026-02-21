5fd28934f17b5ed7084d93dc1d2b13adacaf6b7d
Nayan Dey <ndeynayan123@gmail.com>
2026-02-22 03:51:52 +0530

    Add status button and implement job availability toggling functionality

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,11 +1,13 @@
-console.log("toggle");
+// console.log("toggle");
 
+
+
+// button toggle
 const allFilterBtn = document.getElementById("all-filter-btn")
 const interviewFilterBtn = document.getElementById("interview-filter-btn")
 const rejectedFilterBtn = document.getElementById("rejected-filter-btn")
 // console.log(rejectedFilterBtn)
 
-// button toggle
 function toggleStyle(id){
     allFilterBtn.classList.remove("btn-primary")
     interviewFilterBtn.classList.remove("btn-primary")
@@ -17,4 +19,29 @@ function toggleStyle(id){
 }
 
 
-// card status toggling
+// no jobs available toggling
+const allCards = document.getElementById("allCards");
+const emptyInterview = document.getElementById("empty-interview")
+const emptyRejected = document.getElementById("empty-rejected")
+
+function hide(){
+    allCards.classList.add("hidden");
+    emptyInterview.classList.add("hidden");
+    emptyRejected.classList.add("hidden");
+}
+
+allFilterBtn.addEventListener("click", function(){
+    hide();
+    allCards.classList.remove("hidden");
+})
+
+interviewFilterBtn.addEventListener("click", function(){
+    hide();
+    emptyInterview.classList.remove("hidden");
+})
+
+rejectedFilterBtn.addEventListener("click", function(){
+    hide();
+    emptyRejected.classList.remove("hidden");
+})
+
